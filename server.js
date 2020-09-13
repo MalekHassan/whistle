d01501b7312b61bae52c242a35411e7a58be16cb
Sayefdeen Alrawad <sayefdeen.alrawad@gmail.com>
2020-09-13 22:55:46 +0300

    get live data from API

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static('./public'));
 
 // Routs
 app.get('/', homePage);
-app.get('/live', getData);
+app.get('/live', getLiveMatches);
 
 // Functions
 
@@ -30,7 +30,7 @@ async function homePage(req, res) {
   const NEWS_API_KEY = process.env.NEWS_API_KEY;
   const todayDate = getTodayDate();
   const newsUrl = `https://newsapi.org/v2/everything?qInTitle="+soccer"&to=${todayDate}&pageSize=30&apiKey=${NEWS_API_KEY}`;
-  let liveMatches = await getLiveMatches(req, res);
+  let liveMatches = await getUpCommingMatches(req, res);
   superagent.get(newsUrl).then((data) => {
     let newsArray = data.body.articles.map((news) => {
       return new News(news);
@@ -43,19 +43,23 @@ async function homePage(req, res) {
 }
 
 // Get Live Soccer Matches From API
-function getData(req, res) {
+function getLiveMatches(req, res) {
   const SOCCER_API_KEY = process.env.SOCCER_API_KEY;
   const todayDate = getTodayDate();
   const liveURL = `https://apiv2.apifootball.com/?action=get_events&from=${todayDate}&to=${todayDate}&APIkey=${SOCCER_API_KEY}`;
   superagent
     .get(liveURL)
     .then((data) => {
-      let array = data.body.filter((item) => {
-        if (item.match_live === '1' && item.match_status !== 'Finished') {
-          return item;
-        }
-      });
-      res.status(200).json(array);
+      let liveMatchesArray = data.body
+        .filter((item) => {
+          if (item.match_live === '1' && item.match_status !== 'Finished') {
+            return item;
+          }
+        })
+        .map((match) => {
+          return new liveMatches(match);
+        });
+      res.render('pages/live', { matchArray: liveMatchesArray });
     })
     .catch((err) => {
       res.status(500).json({
@@ -65,7 +69,7 @@ function getData(req, res) {
 }
 
 // Get UpComming Matches From API
-async function getLiveMatches(req, res) {
+async function getUpCommingMatches(req, res) {
   const { league_id } = req.query;
   const SOCCER_API_KEY = process.env.SOCCER_API_KEY;
   const todayDate = getTodayDate();
@@ -77,7 +81,7 @@ async function getLiveMatches(req, res) {
     if (data.body.length > 0) {
       return data.body
         .map((match) => {
-          return new LiveMatch(match);
+          return new UpCommingMatches(match);
         })
         .sort((a, b) => {
           if (a.time < b.time) {
@@ -98,7 +102,6 @@ async function getLiveMatches(req, res) {
 //News Constructors - #1
 function News(newsData) {
   this.title = newsData.title;
-  // this.image_url = newsData.urlToImage;
   this.image_url = newsData.urlToImage
     ? newsData.urlToImage.includes('rcom-default')
       ? '/images/background.jpg'
@@ -109,13 +112,27 @@ function News(newsData) {
   this.description = newsData.description;
 }
 
-// Main page Live constructor
-function LiveMatch(matchData) {
+// Main page UpCommingMatches constructor
+function UpCommingMatches(matchData) {
   this.time = matchData.match_time;
   this.home_team = matchData.match_hometeam_name;
   this.away_team = matchData.match_awayteam_name;
   this.home_team_logo = matchData.team_home_badge;
   this.away_team_logo = matchData.team_away_badge;
+}
+
+// Live Matches Constructor
+
+function liveMatches(matchData) {
+  this.match_id = matchData.match_id;
+  this.league_name = matchData.league_name;
+  this.match_time = matchData.match_time;
+  this.match_hometeam_name = matchData.match_hometeam_name;
+  this.match_awayteam_name = matchData.match_awayteam_name;
+  this.team_home_badge = matchData.team_home_badge;
+  this.team_away_badge = matchData.team_away_badge;
+  this.match_hometeam_score = matchData.match_hometeam_score;
+  this.match_awayteam_score = matchData.match_awayteam_score;
 }
 
 // Listen To Server
