1dda287da6fd4f119369217d78dafd01c6d1d915
Sayefdeen Alrawad <sayefdeen.alrawad@gmail.com>
2020-09-14 14:10:13 +0300

    working on challenge question

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-"use strict";
+'use strict';
 
 // Dependencies
 
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const PORT = process.env.PORT || 3030;
 const app = express();
-const superagent = require("superagent");
-app.set("view engine", "ejs");
+const superagent = require('superagent');
+app.set('view engine', 'ejs');
 
 // Using the public folder and sunFiles
-app.use(express.static("./public"));
+app.use(express.static('./public'));
 
 // Routs
-app.get("/", homePage);
-app.get("/live", getLiveMatches);
-app.get("/match_detail/:matchID", getLiveMatchDetails);
+app.get('/', homePage);
+app.get('/live', getLiveMatches);
+app.get('/match_detail/:matchID', getLiveMatchDetails);
+app.get('/question', getQuestionsChall);
 
 // Functions
 
 // Get Today date
 
 function getTodayDate() {
-  return new Date().toJSON().slice(0, 10).replace(/-/g, "/");
+  return new Date().toJSON().slice(0, 10).replace(/-/g, '/');
 }
 
 // Home page function
@@ -36,7 +37,7 @@ async function homePage(req, res) {
     let newsArray = data.body.articles.map((news) => {
       return new News(news);
     });
-    res.render("pages/index", {
+    res.render('pages/index', {
       news: newsArray,
       matches: liveMatches,
     });
@@ -56,7 +57,7 @@ async function getLiveMatches(req, res) {
       let liveMatchesArray = data.body
         .filter((item) => {
           // item.match_live === '1'
-          if (item.match_status !== "Finished") {
+          if (item.match_status !== 'Finished') {
             return item;
           }
         })
@@ -64,11 +65,11 @@ async function getLiveMatches(req, res) {
           return new liveMatches(match);
         });
       // console.log(liveMatchesArray);
-      res.render("pages/live", { matchArray: liveMatchesArray });
+      res.render('pages/live', { matchArray: liveMatchesArray });
     })
     .catch((err) => {
       res.status(500).json({
-        error: "Somthing went bad",
+        error: 'Somthing went bad',
       });
     });
 }
@@ -78,8 +79,9 @@ async function getUpCommingMatches(req, res) {
   const { league_id } = req.query;
   const SOCCER_API_KEY = process.env.SOCCER_API_KEY;
   const todayDate = getTodayDate();
-  const liveURL = `https://apiv2.apifootball.com/?action=get_events&from=${todayDate}&to=${todayDate}&league_id=${league_id || 148
-    }&APIkey=${SOCCER_API_KEY}`;
+  const liveURL = `https://apiv2.apifootball.com/?action=get_events&from=${todayDate}&to=${todayDate}&league_id=${
+    league_id || 148
+  }&APIkey=${SOCCER_API_KEY}`;
 
   let matchesArray = await superagent.get(liveURL).then((data) => {
     if (data.body.length > 0) {
@@ -108,12 +110,23 @@ function getLiveMatchDetails(req, res) {
   const todayDate = getTodayDate();
   const SOCCER_API_KEY = process.env.SOCCER_API_KEY;
   const matchResultUrl = `https://apiv2.apifootball.com/?action=get_events&from=${todayDate}&to=${todayDate}&match_id=${matchID}&APIkey=${SOCCER_API_KEY}`;
-  console.log("getLiveMatchDetails -> matchResultUrl", matchResultUrl)
-  return superagent.get(matchResultUrl)
-    .then(data => {
-      let matchDetail = new MatchDetail(data.body[0]);
-      res.render("pages/matchDetails", { match: matchDetail });
-    })
+  console.log('getLiveMatchDetails -> matchResultUrl', matchResultUrl);
+  return superagent.get(matchResultUrl).then((data) => {
+    let matchDetail = new MatchDetail(data.body[0]);
+    res.render('pages/matchDetails', { match: matchDetail });
+  });
+}
+
+// Challenge Questions
+
+function getQuestionsChall(req, res) {
+  // console.log(req.query);
+  const { question, diffculty } = req.query;
+  const questionURL = `https://opentdb.com/api.php?amount=${question}&category=21&difficulty=${diffculty}&type=multiple`;
+  console.log(questionURL);
+  superagent.get(questionURL).then((data) => {
+    console.log(data.body.results);
+  });
 }
 
 //Constructors
@@ -122,10 +135,10 @@ function getLiveMatchDetails(req, res) {
 function News(newsData) {
   this.title = newsData.title;
   this.image_url = newsData.urlToImage
-    ? newsData.urlToImage.includes("rcom-default")
-      ? "/images/background.jpg"
+    ? newsData.urlToImage.includes('rcom-default')
+      ? '/images/background.jpg'
       : newsData.urlToImage
-    : "/images/background.jpg";
+    : '/images/background.jpg';
   this.url = newsData.url;
   this.author = newsData.author;
   this.description = newsData.description;
@@ -151,10 +164,10 @@ function liveMatches(matchData) {
   this.match_awayteam_name = matchData.match_awayteam_name;
   this.team_home_badge = matchData.team_home_badge
     ? matchData.team_home_badge
-    : "https://apiv2.apifootball.com/badges/17691_hafnarfjordur-w.png";
+    : 'https://apiv2.apifootball.com/badges/17691_hafnarfjordur-w.png';
   this.team_away_badge = matchData.team_away_badge
     ? matchData.team_away_badge
-    : "https://apiv2.apifootball.com/badges/17691_hafnarfjordur-w.png";
+    : 'https://apiv2.apifootball.com/badges/17691_hafnarfjordur-w.png';
   this.score = `${matchData.match_hometeam_score} : ${matchData.match_awayteam_score}`;
 }
 
@@ -173,7 +186,7 @@ function MatchDetail(matchData) {
   this.league_logo = matchData.league_logo;
   this.cards = matchData.cards; /*array*/
   this.substitutions = matchData.substitutions;
-  this.score = `${matchData.match_hometeam_score} : ${matchData.match_awayteam_score}`
+  this.score = `${matchData.match_hometeam_score} : ${matchData.match_awayteam_score}`;
 }
 
 // Listen To Server
